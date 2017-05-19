f81bc885d391aff8043b8e0781a4591a47fc691d
李全城 <liquanchen9@sina.com>
2017-05-19 06:41:38 +0000

    change ajax mock!

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -4,7 +4,7 @@ export default ApplicationAdapter.extend({
     
     urlForFindAll() {
         //记得这里是返回 整体的 url 如果有配置namaspace就要设置体现上 
-        return `${this.namespace}/users.form`;
+        return `${this.namespace}/users.json`;
     }
     
       
